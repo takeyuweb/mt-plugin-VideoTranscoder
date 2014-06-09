a55f71cd21534f351ec6df62e1c5881538157060
Yuichi Takeuchi <uzuki05@takeyu-web.com>
2014-06-09 22:31:18 +0900

    日本語訳

--- plugins/VideoTranscoder/lib/VideoTranscoder/L10N/ja.pm
+++ plugins/VideoTranscoder/lib/VideoTranscoder/L10N/ja.pm
@@ -6,6 +6,12 @@ use vars qw(%Lexicon);
 %Lexicon = (
     '__PLUGIN_DESCRIPTION__'    => 'アイテムにAmazon ElasticTranscoderを利用した動画変換機能を追加します。',
     'Video Transcoding'         => '動画変換',
+    'Create Job'                => '登録',
+    'VideoTranscoder Jobs'      => '変換キュー',
+    'Job ID'                    => 'ジョブID',
+    'Pipeline'                  => 'パイプライン',
+    'Preset'                    => 'プリセット',
+    'Job Status'                => '状態',
 );
 
 1;
